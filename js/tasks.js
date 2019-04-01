ee527728cdcfeaf7d113a7fda223896797a944f7
Hannah Kim <hk21042@gmail.com>
2019-04-01 16:32:01 +0000

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -1,14 +1,18 @@
 function createTask() {
   localStorage.setItem("tasks", String(document.getElementById('taskTable').innerHTML));
   var taskName = document.getElementById('task_name').value;
+  var taskDes = document.getElementById('task_descrip').value;
   //var taskPerson = document.getElementById('task_person').value;
   //var dueDate = document.getElementById('task_date').value;
   //document.getElementById("taskDone").preventDefault()
   var taskContents = `
         <tr>
           <td width="10%">
-            <p style="font-size:30px">
+            <h2>
               ${taskName}
+            </h2>
+            <p>
+              ${taskDes}
             </p>
           </td>
         </tr>`
