7239e3dbb17c24d5461ef61d81e3c7e975a556b6
Hannah Kim <hk21042@gmail.com>
2019-04-01 16:20:42 +0000

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -1,8 +1,8 @@
 function createTask() {
   localStorage.setItem("tasks", String(document.getElementById('taskTable').innerHTML));
   var taskName = document.getElementById('task_name').value;
-  var taskPerson = document.getElementById('task_person').value;
-  var dueDate = document.getElementById('task_date').value;
+  //var taskPerson = document.getElementById('task_person').value;
+  //var dueDate = document.getElementById('task_date').value;
   //document.getElementById("taskDone").preventDefault()
   var taskContents = `
         <tr>
@@ -10,24 +10,6 @@ function createTask() {
             <p style="font-size:30px">
               ${taskName}
             </p>
-          </td>
-          <td width="15%">
-            <img src = "media/cove.jpg" class="profilepic1" align = center>&nbsp;&nbsp;&nbsp;${taskPerson}
-          </td>
-          <td width="25%">
-            <div class="taskBox" style="background-color:pink">
-              need help
-            </div>
-          </td>
-          <td width="25%">
-            <div class="taskBox" style="background-color:orange">
-              medium
-            </div>
-          </td>
-          <td width="25%">
-            <div class="taskBox" style="background-color:lightgray">
-              ${dueDate}
-            </div>
           </td>
         </tr>`
   localStorage.tasks = localStorage.getItem("tasks") + taskContents;
