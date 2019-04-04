7bf2bc14e2161cb33bc17904359321eaddad79cd
Michelle Liu <mliuu3259@gmail.com>
2019-04-04 03:00:56 +0000

    Made theme apply to home

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
     <link type = "text/css" rel="stylesheet" href="css/style.css" id="theme">
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
   </head>
-  <body onload="loadProjects()">
+  <body onload="loadProjects(), setTheme()">
       <!--<?php if  (isset($_SESSION['success'])): ?>-->
         <div class = "content">
           <div class="sidenav">
