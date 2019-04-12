7f42826ff6988b596f2d981a29ad3d600de55b67
Alex Liu <liu.alex.000@gmail.com>
2019-04-12 16:42:39 +0000

    Update js/script.js

--- js/script.js
+++ js/script.js
@@ -93,6 +93,6 @@ function register() {
   localStorage.setItem("name", document.getElementById('name').value);
   localStorage.setItem("email", document.getElementById('email').value);
   localStorage.setItem("password", document.getElementById('password_1').value);
-  document.location.href = https://hannah.kim.gitlab.io/collabcove/home.html;
+  //document.location.href = https://hannah.kim.gitlab.io/collabcove/home.html;
   console.log("x");
 }
